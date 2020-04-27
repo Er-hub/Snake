77e3dee0c5d83997748c00df3d6be0240a922dea
Er <andreyy2000@inbox.ru>
2020-04-27 16:15:10 +0300

    add list

---
+++ Snake/Horizontalline.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Horizontalline : Figure
+    {
+        public Horizontalline(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public override void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
